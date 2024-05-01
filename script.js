564aec600e3ad31923130032de7a152e82f6ee7a
Dipto Neogi <81651700+Dipto07@users.noreply.github.com>
2024-05-01 06:44:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 //your JS code here. If required.
-function playSound(soundName) {
-    var sound = document.getElementById(soundName);
-    if (sound) {
-        sound.currentTime = 0;
-        sound.play();
-    }
-}
-
-function stopAllSounds() {
-    var sounds = document.querySelectorAll("audio");
-    sounds.forEach(function(sound) {
-        sound.pause();
-        sound.currentTime = 0;
-    });
-}
-
+function sound() {
+	let music = document.getElementById("audio");
+	music.play();
+} 
